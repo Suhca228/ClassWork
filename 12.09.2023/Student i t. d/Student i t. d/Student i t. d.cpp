98f35d4eb1f910b36a5d3caa1d04cbd08f4558c3
Suhca228 <vovak9855@gmail.com>
2023-09-12 20:57:42 +0300

    12.09.2023

---
+++ 12.09.2023/Student i t. d/Student i t. d/Student i t. d.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Student {
+public:
+    //Властивості
+    string name;
+    int age;
+    //Метод
+    void sayHi() {
+        cout << "Hello? my name is" << name;
+    }
+};
+
+
+Student olderStudent(Student newList[], int size) {
+    Student oldest = newList[0];
+    
+    for (int i = 1; i < size; i++) {
+        if (newList[i].age > oldest.age) {
+            oldest = newList[i];
+        }
+    }
+    
+    return oldest;
+}
+
+int main() {
+    Student newList[3]{ {"Vlad",18},
+                       {"Maxim",16},
+                       {"Oleg",20} };
+    Student oldestStudent = olderStudent(newList, 3);
+    
+    cout << "Найстарший: " << oldestStudent.name << endl;
+    
+}
+
